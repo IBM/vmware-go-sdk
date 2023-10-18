d7e1d09dd3058b9aacc061916e37cb699e24865e
Boyang Zhang <Bo.Yang.Zhang1@ibm.com>
2023-10-18 16:38:12 +0800

    fix wrong import issue

--- vmwarev1/vmware_v1_test.go
+++ vmwarev1/vmware_v1_test.go
@@ -27,9 +27,10 @@ import (
 	"time"
 
 	"github.com/IBM/go-sdk-core/v5/core"
-	"github.com/IBM/vmware-go-sdk"
+	"github.com/IBM/vmware-go-sdk/vmwarev1"
 	"github.com/go-openapi/strfmt"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe(`VmwareV1`, func() {
